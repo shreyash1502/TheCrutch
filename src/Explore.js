2b7576b7916cc2894a2b4148bd70726855c4d381
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-03-25 01:20:35 +0530

    Update Explore.js

--- src/Explore.js
+++ src/Explore.js
@@ -1,29 +1,76 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { auth, provider , db} from './firebase';
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import DataCard from './DataCard'
+import NavbarComponent from './NavbarComponent';
 function Explore() {
-    const handleSubmit=()=>{
-        db.collection("Available").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                // console.log(doc.data().mail);
-                // console.log(doc.data().name);
-            });
-        });
+    const [equipmentList, setEquipments]=useState([]);
+    const fetchData = async () => {
+        const res = db.collection('Available');
+        const data = await res.get();
+
+        setEquipments(data.docs.map(item => item.data()))
     }
-    return (
-        <div>
-            <Link to="/data">
-            <button onClick={handleSubmit}>show</button>
-            </Link>
-        </div>
+    useEffect(()=>{
+        fetchData();
+    },[])
+    equipmentList.forEach(item=>{
+        //console.log(item.description);
+    })
+
+  
+    return ( 
+        <>   
+        <NavbarComponent style={{position:"Sticky"}}/>
+       { equipmentList && equipmentList.map(eq=>(
+        <div class="card-group">
+        
+        
+        <div class="col-lg-6">
+        
+           <div  key={eq.uid}>
+           <p> 
+               <DataCard 
+                    
+                    equip={eq.equipment}
+                    desc={eq.description}
+                    /></p>
+                    </div>
+           
+           
+           </div>
+           </div>
+       ))
+       
+   }
+   <div class="card-group">
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+    </>
     )
+
 }
 
 export default Explore
