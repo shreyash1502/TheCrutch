c30943e1b1b5a5c761c7042c9a1101a60cf3f3c8
Shreyash Vardhan <shreyashfeb@gmail.com>
2021-03-26 20:21:42 +0530

    changed home page

--- src/Components/HomeUI.js
+++ src/Components/HomeUI.js
@@ -3,12 +3,11 @@ import '.././Styles/HomeUI.css'
 function HomeUI() {
     return (
         <div className="container"> 
-        <div className="contain">   
-            <div className="back"> 
-            {/* <div className="doc">
-               </div> */}
-               </div>
-               </div>
+      
+     
+      <div className="back">
+           
+           </div>
             <div className="hmbutton">
             </div>
             <div className="titleHome"></div>
@@ -17,10 +16,16 @@ function HomeUI() {
                
                <div className="welcometext"></div>
               <div className="knowmore"></div>
-               <div className="how">
-                   
-               </div>
-        
+                <div className="howitworks"></div>
+               <div className="component4"></div>
+               <p>You donate eqipment</p>
+                <div className="component5"></div>
+                <div className="seekservice"></div>
+               
+                <div className="comp6">
+                  <p>Seek our Services as </p>
+                </div>
+                <div className="comp9"></div>
         </div>
     )
 }
