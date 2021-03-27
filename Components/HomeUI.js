8ef5e25de4fa712b47f1eb2afbb46130b3443546
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-03-27 15:22:48 +0530

    Add files via upload

---
+++ Components/HomeUI.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '.././Styles/HomeUI.css'
+function HomeUI() {
+    return (
+        <div className="container"> 
+      
+     
+      <div className="back">
+           
+           </div>
+            <div className="hmbutton">
+            </div>
+            <div className="titleHome"></div>
+               <div className="mission">
+               </div>
+               <div className="openpic"></div>
+               <div className="welcometext"></div>
+              <div className="knowmore"></div>
+                <div className="howitworks"></div>
+               <div className="component4"></div>
+               <p>You donate equipment</p>
+                <div className="component5"></div>
+
+                <div className="seekservice"></div>
+               <div className="component9">
+              </div>
+
+                <div className="comp6">
+                  
+                </div>
+                <div className="component7">
+                  
+                </div>
+
+        </div>
+    )
+}
+
+export default HomeUI
